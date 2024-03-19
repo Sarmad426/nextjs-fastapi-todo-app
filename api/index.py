aec37ac66cde52b5a3d6eed20fc410dbe875af78
Sarmad rafique <sarmadrafique040@gmail.com>
2024-03-19 07:34:44 +0500

    Created neon postgres connection

--- api/index.py
+++ api/index.py
@@ -1,30 +1,48 @@
 from typing import Optional
 
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request, Form, Depends,HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi.responses import HTMLResponse
 from typing import Annotated
+from .settings import DATABASE_URL
 
 class Todo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(index=True)
     completed: bool = False
 
+connection_string = str(DATABASE_URL).replace(
+"postgresql", "postgresql+psycopg2"
+)
 
-sqlite_file_name = "todos.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# engine = create_engine(
+#     connection_string, pool_recycle=300
+# )
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+# connect_args = {"check_same_thread": False}
+engine = create_engine(connection_string)
 
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Creating tables..")
+    create_db_and_tables()
+    yield
 
+
+app = FastAPI(lifespan=lifespan, title="Hello World API with DB", 
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://localhost:3000", # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server"
+        }
+        ])
 
 @app.on_event("startup")
 def on_startup():
